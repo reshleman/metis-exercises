d5585b928c4aa90577fede74b8684b635fb1c857
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-06-18 17:01:45 -0400

    Add description comment to lockers problem

--- lockers.rb
+++ lockers.rb
@@ -1,3 +1,7 @@
+# Imagine a school that has 100 closed lockers and 100 students standing in line. The 1st student goes through and opens every locker, the 2nd student closes every 2nd locker, and the 3rd student toggles every 3rd locker. If the remaining students go through, likewise, and toggle the state of any locker that is a multiple of their number in line, what lockers are open at the end?
+#
+# Print locker numbers from 1-100, as well as its final state ("open" or "closed").
+
 class LockerProblem
   def initialize
     @lockers = Array.new(101, "closed")
