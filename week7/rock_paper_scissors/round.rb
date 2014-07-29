81356f3a9b50a25a6506facdaacfac54885fc492
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-29 15:52:58 -0400

    Refactor and validate user's move

--- week7/rock_paper_scissors/round.rb
+++ week7/rock_paper_scissors/round.rb
@@ -1,36 +1,40 @@
 class Round
   def initialize
-    @user_move = ""
-    @ai_move = ""
+    @user_move = get_user_move until valid_move?
+    @ai_move = get_ai_move
   end
 
   def play
-    user_move
-    ai_move
     print_result
   end
 
   private
 
+  attr_reader :user_move, :ai_move
+
   VALID_MOVES = %w(R P S)
 
-  def user_move
+  def valid_move?
+    VALID_MOVES.include?(user_move)
+  end
+
+  def get_user_move
     print "Your move? (R/P/S, q to quit) > "
     @user_move = gets.chomp.upcase
   end
 
-  def ai_move
+  def get_ai_move
     @ai_move = VALID_MOVES[rand(VALID_MOVES.count)]
-    puts "AI played #{@ai_move}"
   end
 
   def print_result
-    if @ai_move == @user_move
+    puts "AI played #{ai_move}"
+    if ai_move == user_move
       puts "Tie"
     elsif(
-      (@ai_move == "P" && @user_move == "R") ||
-      (@ai_move == "R" && @user_move == "S") ||
-      (@ai_move == "S" && @user_move == "P")
+      (ai_move == "P" && user_move == "R") ||
+      (ai_move == "R" && user_move == "S") ||
+      (ai_move == "S" && user_move == "P")
     )
       puts "AI wins"
     else
